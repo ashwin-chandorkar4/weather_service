9859daf884ba93aa4435d8186f5434ef30290049
winash2510 <ashwin.chandorkar4@gmail.com>
2024-02-01 03:56:34 +0530

    weather of user specified cities

--- src/main/java/com/example/weatherapidemo/controller/WeatherController.java
+++ src/main/java/com/example/weatherapidemo/controller/WeatherController.java
@@ -3,6 +3,7 @@ package com.example.weatherapidemo.controller;
 import com.example.weatherapidemo.service.WeatherService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,14 +21,14 @@ public class WeatherController {
         return "Default endpoint";
     }
     @GetMapping("/summary")
-    public String getWeatherSummary() {
-        String weatherData = weatherService.getWeatherData();
+    public String getWeatherSummary(@RequestParam String city) {
+        String weatherData = weatherService.getWeatherData(city);
         return "Weather Summary: " + weatherData;
     }
 
     @GetMapping("/hourly")
-    public String getHourlyWeather() {
-        String hourlyData = weatherService.getHourlyWeather();
+    public String getHourlyWeather(@RequestParam String city) {
+        String hourlyData = weatherService.getHourlyWeather(city);
         return "Hourly Weather: " + hourlyData;
     }
 }
